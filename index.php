19338ab0f6152607b8ed26cca0ef546fc11c504b
Lotus <154233234+Fietsbel12@users.noreply.github.com>
2025-11-04 19:48:41 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Asset&family=Permanent+Marker&family=Sixtyfour&family=VT323&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Portfolio</title>
+</head>
+
+    <body>
+        <header>
+            <?php require_once 'php/header.php';?>
+        </header>
+
+        <body>
+            <div class="wrapper">
+                <div id="intro-screen">
+                    <div class="crt">
+                        <p>☆✿ Welcome to the LO DEVIGN dimension ✿☆</p>
+                        <p class="blink">Press [Enter] to connect</p>
+                    </div>
+                </div>
+            </div>
+
+            <script src="js/enter.js"></script>
+        </body>
+
+        <footer>
+            <?php require_once("php/footer.php");?>
+        </footer>
+    </body>
+</html>
